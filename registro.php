18b03e2236357e071ef213667f4797a2e3f79c39
Diego Lopez Garro <114270024+dalg97@users.noreply.github.com>
2025-08-09 23:44:53 -0600

    Agregando el head en el registro

--- registro.php
+++ registro.php
@@ -5,6 +5,9 @@
   <title>Registro de Usuario</title>
   <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>VolunTeam - Registro</title>
+  <link rel="stylesheet" href="styles.css" />
 </head>
 <body>
 

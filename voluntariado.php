05db9e6e125fe317f2b9814c3eaac8f637cd4497
Diego Lopez Garro <114270024+dalg97@users.noreply.github.com>
2025-08-12 23:56:18 -0600

    agregando el backend al html

--- voluntariado.php
+++ voluntariado.php
@@ -1,13 +1,4 @@
-<?php
-
-$conexion = new mysqli("localhost", "volunt_user", "jdu2352h", "volunteam");
-if ($conexion->connect_error) {
-    die("Conexión fallida: " . $conexion->connect_error);
-}
-
-$sql = "SELECT * FROM evento WHERE destacado=FALSE";
-$resultado = $conexion->query($sql);
-?>
+<?php require_once "scripts/procesar_eventos.php" ?>
 
 <!DOCTYPE html>
 <html lang="es">
